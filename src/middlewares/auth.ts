4f678a211d20b3e1002feb5646a71cf25927bd41
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-09-21 18:08:11 +0330

    feat: create auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import httpErrors from "http-errors";
+import AdminModel from "../models/Admin";
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { accessToken } = req.cookies;
+
+    if (!accessToken) {
+      throw httpErrors.Forbidden(
+        "این مسیر محافظت شده است لطفا برای دسترسی به این مسیر اول وارد شوید."
+      );
+    }
+    const { ACCESS_TOKEN_SECRET_KEY } = process.env;
+    const checkToken: any = jwt.verify(
+      accessToken,
+      ACCESS_TOKEN_SECRET_KEY as string
+    );
+
+    const admin = await AdminModel.findOne({ where: { id: checkToken.id } });
+
+    if (!admin) {
+      throw httpErrors.NotFound("مدیری پیدا نشد");
+    }
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import { auth } from '../controllers/auth';
 
 const router = express.Router()
 
-router.post("/")
+router.post("/" , auth)
 
 export default router
