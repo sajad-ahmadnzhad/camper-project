4d3994a14f982fd675f5b3092fd531331aebe6bc
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-09-27 13:31:50 +0330

    feat: create social model

---
+++ src/models/Social.ts
@@ -0,0 +1,28 @@
+import { DataTypes } from "sequelize";
+import db from "../configs/db";
+
+const Social = db.define("Social", {
+  id: {
+    type: DataTypes.NUMBER,
+    unique: true,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  platform: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  url: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  icon: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+export default Social;
