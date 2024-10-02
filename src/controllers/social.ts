5e85362b7a0accf921f89f2c0252ded0d85e8f59
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-10-02 23:07:13 +0330

    feat: created all apis for social

--- src/controllers/social.ts
+++ src/controllers/social.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import SocialModel from "../models/Social";
 import httpErrors from "http-errors";
+import pagination from "../utils/pagination";
 
 export const create = async (
   req: Request,
@@ -30,6 +31,24 @@ export const update = async (
   next: NextFunction
 ) => {
   try {
+    const { platform } = req.body;
+
+    const existingSocial = await SocialModel.findOne({
+      where: { platform },
+    });
+
+    if (!existingSocial) {
+      throw new httpErrors.Conflict("این شبکه اجتماعی از قبل وجود دارد.");
+    }
+
+    const updatedSocial = await SocialModel.update(
+      {
+        where: { id: existingSocial.dataValues.id },
+      },
+      req.body
+    );
+
+    res.json(updatedSocial);
   } catch (error) {
     next(error);
   }
@@ -41,6 +60,8 @@ export const getAll = async (
   next: NextFunction
 ) => {
   try {
+    const socials = await pagination(SocialModel, req.query);
+    res.json(socials);
   } catch (error) {
     next(error);
   }
@@ -52,6 +73,19 @@ export const getOne = async (
   next: NextFunction
 ) => {
   try {
+    const { id } = req.params;
+
+    if (!Number(id)) {
+      throw new httpErrors.BadRequest("آیدی ارسال شده نامعبتر می باشد.");
+    }
+
+    const social = await SocialModel.findOne({ where: { id } });
+
+    if (!social) {
+      throw new httpErrors.NotFound("شبکه اجتماعی پیدا نشد.");
+    }
+
+    res.json(social.dataValues);
   } catch (error) {
     next(error);
   }
